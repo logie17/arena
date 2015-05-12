433cc39be3c0c88472b16d914309bdb6e707aac5
Logan Bell <loganbell@gmail.com>
2015-05-11 22:51:48 -0700

    fmt

--- client/main.go
+++ client/main.go
@@ -1,22 +1,21 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"github.com/logie17/arena/client/fighter"
 	"github.com/nsf/termbox-go"
 	"net"
-	"strings"
-	"strconv"
-	"fmt"
-	"bufio"
-	"time"
-	"github.com/logie17/arena/client/fighter"
 	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
 const (
 	boardWidth  = 79
 	boardHeight = 30
 )
-
 
 func print_msg(x, y int, fg, bg termbox.Attribute, msg string) {
 	for _, c := range msg {
@@ -29,13 +28,13 @@ func readFromServer(fighterId int, fighters []fighter.Fighter, bufc *bufio.Reade
 	go func() {
 		for {
 			line, _ := bufc.ReadString('\n')
-			str := strings.Split(strings.TrimSpace(string(line)),",");
-			id,_ := strconv.Atoi(str[1])
+			str := strings.Split(strings.TrimSpace(string(line)), ",")
+			id, _ := strconv.Atoi(str[1])
 
 			if id != fighterId && isNewEnemy(id, fighters) {
-				x,_ := strconv.Atoi(str[2])
-				y,_ := strconv.Atoi(str[3])
-				enemy := fighter.NewFighter(x,y,id,"enemy",reply)
+				x, _ := strconv.Atoi(str[2])
+				y, _ := strconv.Atoi(str[3])
+				enemy := fighter.NewFighter(x, y, id, "enemy", reply)
 				fighters = append(fighters, enemy)
 			}
 
@@ -67,10 +66,9 @@ func handleFighterActions(cn net.Conn, reply chan fighter.CommandData) {
 				x := val[1]
 				y := val[2]
 
-
-				if (action == "FLUSH") {
+				if action == "FLUSH" {
 					termbox.Flush()
-				} else if ( action == "HIT") {
+				} else if action == "HIT" {
 					termbox.SetCell(x, y, '@', termbox.ColorYellow, termbox.ColorBlack)
 					termbox.Flush()
 					go func() {
@@ -79,10 +77,10 @@ func handleFighterActions(cn net.Conn, reply chan fighter.CommandData) {
 						termbox.Flush()
 
 					}()
-				} else if ( action == "HIDE" ) {
+				} else if action == "HIDE" {
 					termbox.SetCell(x, y, ' ', termbox.ColorBlack, termbox.ColorBlack)
 					termbox.Flush()
-				} else if ( action == "DRAW" ) {
+				} else if action == "DRAW" {
 					enemy := val[3]
 					if enemy == 1 {
 						termbox.SetCell(x, y, '@', termbox.ColorRed, termbox.ColorBlack)
@@ -93,11 +91,10 @@ func handleFighterActions(cn net.Conn, reply chan fighter.CommandData) {
 				} else {
 					cn.Write([]byte(fmt.Sprintf("%s,%d,%d,%d\n", action, id, x, y)))
 				}
-				
+
 			}
 		}
 	}()
-
 
 }
 
@@ -105,8 +102,8 @@ func setupBoard() {
 	termbox.SetInputMode(termbox.InputEsc | termbox.InputMouse)
 	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
 
-	print_msg(int(boardWidth/2) - (int(boardWidth/2)/2), 0, termbox.ColorRed, termbox.ColorBlack, "ARENA!!! FIGHT!!!")
-	
+	print_msg(int(boardWidth/2)-(int(boardWidth/2)/2), 0, termbox.ColorRed, termbox.ColorBlack, "ARENA!!! FIGHT!!!")
+
 	// termbox.SetCell(0, 2, 0x250C, termbox.ColorRed, termbox.ColorBlack)
 	// termbox.SetCell(boardWidth+1, 0, 0x2510, termbox.ColorRed, termbox.ColorBlack)
 	// termbox.SetCell(0, boardHeight+1, 0x2514, termbox.ColorRed, termbox.ColorBlack)
@@ -139,13 +136,13 @@ func main() {
 
 	defer termbox.Close()
 	setupBoard()
-	
-	destination := "127.0.0.1:9000";
-	cn, err := net.Dial("tcp", destination);
+
+	destination := "127.0.0.1:9000"
+	cn, err := net.Dial("tcp", destination)
 	if err != nil {
 		os.Exit(1)
 	}
-	
+
 	//defer cn.Close();
 
 	bufc := bufio.NewReader(cn)
@@ -157,8 +154,8 @@ func main() {
 	if err != nil {
 		os.Exit(1)
 	}
-	
-	str := strings.Split(strings.TrimSpace(string(line)),",");
+
+	str := strings.Split(strings.TrimSpace(string(line)), ",")
 
 	fighterId, err := strconv.Atoi(str[1])
 	if err != nil {
@@ -168,13 +165,13 @@ func main() {
 	if err != nil {
 		os.Exit(1)
 	}
-	y,err := strconv.Atoi(str[3])
+	y, err := strconv.Atoi(str[3])
 	if err != nil {
 		os.Exit(1)
 	}
-	
-	reply := make(chan fighter.CommandData,4)
-	player := fighter.NewFighter(x,y,fighterId,"me", reply)
+
+	reply := make(chan fighter.CommandData, 4)
+	player := fighter.NewFighter(x, y, fighterId, "me", reply)
 	fighters := []fighter.Fighter{player}
 
 	readFromServer(fighterId, fighters, bufc, reply)
